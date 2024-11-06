738b54d9afadde834735ae4e166bb389764c6945
chasty2 <email@codyhasty.net>
2024-11-06 15:15:06 -0600

    style: format comments in vm module

--- proxmox/modules/proxmox_vm_qemu/main.tf
+++ proxmox/modules/proxmox_vm_qemu/main.tf
@@ -9,11 +9,14 @@ terraform {
 
 resource "proxmox_vm_qemu" "virtual_machine" {
   name = var.name
-  count = 1 # Establishes how many instances will be created 
   target_node = var.proxmox_host
 
-  # References our vars.tf file to plug in our template name
+  # Establishes how many instances will be created
+  count = 1
+
+  # Image to clone VM from
   clone = var.template_name
+
   # Creates a full clone, rather than linked clone 
   # https://pve.proxmox.com/wiki/VM_Templates_and_Clones
   full_clone  = "true"
@@ -25,21 +28,23 @@ resource "proxmox_vm_qemu" "virtual_machine" {
   sockets = 1
   memory = var.memory
   vmid = var.vmid
-  # bootdisk = "scsi0"
   scsihw = "virtio-scsi-single"
   ipconfig0 = var.ip_config
 
   disk {
     slot = "scsi0"
     type = "disk"
-    storage = var.storage_pool # Name of storage local to the host you are spinning the VM up on
+    # Name of storage local to the host you are spinning the VM up on
+    storage = var.storage_pool
+    # Disk size of new VM (image is currently 32GB with LVM setup)
     size = var.template_disk_size
   }
 
   network {
     model = "virtio"
     bridge = var.nic_name
-    macaddr = var.macaddr # MAC-associated IP address
-    # tag = var.vlan_num # This tag can be left off if you are not taking advantage of VLANs
+    # MAC-associated IP address
+    macaddr = var.macaddr
+    # tag = var.vlan_num
   }
 }
