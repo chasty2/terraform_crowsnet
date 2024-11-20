c1b9ee41a66943d83d35280467bd35b005db11a8
chasty2 <email@codyhasty.net>
2024-11-20 11:10:22 -0600

    feat: Configure azure virtual network

---
+++ azure/main.tf
@@ -0,0 +1,27 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "crowsnet" {
+  name     = "CrowsNet"
+  location = "eastus"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+    name                = "crowsnet_virtual_network"
+    address_space       = ["192.168.4.0/22"]
+    location            = "eastus"
+    resource_group_name = azurerm_resource_group.crowsnet
+}
