e48338f379db5b89d39e48b48b29c3cf5a3399f5
chasty2 <email@codyhasty.net>
2024-11-25 17:21:43 -0600

    chore: Remove unused outputs

--- azure/outputs.tf
+++ azure/outputs.tf
@@ -2,6 +2,6 @@ output "resource_group_id" {
   value = azurerm_resource_group.crowsnet.id
 }
 
-output "public_ip_address" {
-  value = azurerm_linux_virtual_machine.crowsnet_vm.public_ip_address
-}
+# output "public_ip_address" {
+#   value = azurerm_linux_virtual_machine.crowsnet_vm.public_ip_address
+# }
