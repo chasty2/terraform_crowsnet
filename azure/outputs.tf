e87a357e91f0000f685c3536bdba13a125eeae9e
chasty2 <email@codyhasty.net>
2024-12-16 16:48:23 -0600

    feat: Output public IPs of VMs

--- azure/outputs.tf
+++ azure/outputs.tf
@@ -2,7 +2,10 @@ output "resource_group_id" {
   value = azurerm_resource_group.crowsnet.id
 }
 
-# TODO: Collect static public IP's 
-# output "public_ip_address" {
-#   value = azurerm_linux_virtual_machine.crowsnet_vm.public_ip_address
-# }
+output "internal_group_public_ips" {
+  value = "${module.internal.public_ips}"
+}
+
+output "public_group_public_ips" {
+  value = "${module.public.public_ips}"
+}
